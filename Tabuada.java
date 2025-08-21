6449340c86acc6d253c1b69d8c15d4b8619152a2
felipe783 <felipebolt2007@Gmail.com>
2025-08-21 08:51:46 -0300

    Create Tabuada.java

---
+++ Tabuada.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n1,num,aux;
+        for(aux=0;aux<=10;aux++){
+            for(num=0;num<=10;num++){
+                System.out.printf("\n%d * %d = %d",aux,num,(aux*num));
+            }
+        }
+    }
+}
