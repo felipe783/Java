ed19f69ae96e156288348c72c52543b86f3398b8
felipe783 <felipebolt2007@Gmail.com>
2025-08-21 08:23:49 -0300

    Update 03.Ex ederson das tentativas.java

--- 03.Ex ederson das tentativas.java
+++ 03.Ex ederson das tentativas.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        int i;
         String senhacerta = "1234";
         String usuariocerto = "admin";
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o usuario:");
-        String usuario = scanner.nextLine();
-        System.out.print("Digite a senha:");
-        String senha = scanner.nextLine();
-
-        if (senha.equals(senhacerta) && usuario.equals(usuariocerto)){ //esse equals pra objetos (String, List, etc). é o ==
-            System.out.println("Senha correta!");
-        }else{
-            System.out.println("Senha incorreta!");
+        for(i=0;i<3;i++){
+            System.out.print("Digite o usuario:");
+            String usuario = scanner.nextLine();
+            System.out.print("Digite a senha:");
+            String senha = scanner.nextLine();
+            //3 tentativas
+            if (senha.equals(senhacerta) && usuario.equals(usuariocerto)){ //esse equals pra objetos (String, List, etc). é o ==
+                System.out.println("Senha correta!");
+                break;
+            }
+            else{
+                System.out.println("Senha incorreta!");
+            }
         }
-
+        if(i<3){
+            System.out.println("Voce entrou no sistema");
+        }
+        else{
+            System.out.println("Bloqueado");
+        }
     }
 }
