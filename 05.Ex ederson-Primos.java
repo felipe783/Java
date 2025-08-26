cc4c56378ffebe93627dde46398204749bc34331
felipe783 <felipebolt2007@Gmail.com>
2025-08-26 08:30:55 -0300

    Update 05.Ex ederson-Primos.java

--- 05.Ex ederson-Primos.java
+++ 05.Ex ederson-Primos.java
@@ -9,18 +9,19 @@ public class Main{
         System.out.println("Fale o segundo intervalo:");
         int intervalo2 = input.nextInt();
         input.nextLine();
-        if(intervalo1>intervalo2){
+        if(intervalo1>intervalo2){ //aqui troca os intervalos pra o intervalo1 ser menor q o 2
             int aux=intervalo1;
             intervalo1=intervalo2;
             intervalo2=aux;
         }
-
-        for(int i=intervalo1;i<intervalo2;i++){
+        //Primos
+        for(int i=intervalo1;i<intervalo2;i++){ 
             boolean primo=true;
+            //contador comça em 2 pra sempre ser diferente de i
             for(int contador=2;contador<i;contador++){ //menor q i pq no if vai entrar como i%i e isso sempre da 0 e vai sair
                 if(i % contador==0){ //Ver se tem mais de 1 divisor
                     primo=false;
-                    break; //sai do loop pra encontrou mais de 1 divisor 
+                    break; //sai do loop pra encontrou mais de 1 divisor
                 }
             }
             if(primo){
