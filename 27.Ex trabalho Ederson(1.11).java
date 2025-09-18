b3c792460f4cacccd9cfbc512bea4b72b276fbd8
felipe783 <felipebolt2007@Gmail.com>
2025-09-18 07:42:19 -0300

    Update 27.Ex trabalho Ederson(1.11).java

--- 27.Ex trabalho Ederson(1.11).java
+++ 27.Ex trabalho Ederson(1.11).java
@@ -1,23 +1,16 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 /*Validador de Senhas: Leia uma senha e verifique critérios (comprimento >=8, tem maiúscula, número) com if-else. Use switch
 para níveis de força. Extensão: Integre em segurança de software para autenticação.*/
-
-/*String[] alfabeto=new String[10];
--isso basicamente separa um espaço na memoria por causa do "NEW",resumindo isso deixa todas as posições vazias,e o usuario
-ira preencher futuramente,sem o new ele ja define os valores nas posição
--Em resumo sem o new é bom vc ja deixar definido os valores
-
-A diferença entre o ' e " é que a simples pega so um caracter e a dupla transforma em string*/
-
 public class Main{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
         char[] numeros = {'0','1','2','3','4','5','6','7','8','9'};
         char[] alfabeto = {'A','B','C','D','E','F','G','H','I','J','K','L','M', 'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         String senha;
-        int i = 0,j=0,cont_Letras=0,cont_Numeros=0;
+        int i = 0,j=0,x=0,cont_Letras=0,cont_Numeros=0;
         //Dados
         System.out.println("Fale sua senha:");
         senha = input.nextLine();
@@ -25,18 +18,20 @@ public class Main{
         if(senha.length()>=8){ //Ve o tamanho ta string
             for(i=0;i<senha.length();i++){
                 letras=senha.charAt(i);
-                for(j=0;j<senha.length();j++){
+                for(j=0;j<alfabeto.length;j++){ //aqui ve as letras
                     if(letras==alfabeto[j]){
                         cont_Letras++;
                     }
-                    if(letras==numeros[j]){
+                }
+                for(x=0;x<numeros.length;x++){ //aqui ve os numeros,é separado pq numeros é menor q alfabeto
+                    if(letras==numeros[x]){
                         cont_Numeros++;
                     }
                 }
             }
-            /*Essa matriz tem a logica do seguinte a variavel letras ta recebendo o um char de senhas na posição i
-            Ja o for de dentro percorre todos os vetores(alfabeto,numeros),e ve se tem algum char(que foi pego no charAt)
-            igual a eles dai aumenta em 1 o contador*/
+           /*Essa matriz tem a logica do seguinte a variavel letras ta recebendo o um char de senhas na posição i
+           Ja o for de dentro percorre todos os vetores(alfabeto,numeros),e ve se tem algum char(que foi pego no charAt)
+           igual a eles dai aumenta em 1 o contador*/
             System.out.printf("\nSua senha tem %d numeros",cont_Numeros);
             System.out.printf("\nSua senha tem %d letras Maiusculas",cont_Letras);
         }
