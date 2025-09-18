2bbabd844f94737e43d029914ebd3dceefc8747b
felipe783 <felipebolt2007@Gmail.com>
2025-09-18 09:12:53 -0300

    Update 19.Ex trabalho Ederson(1.3).java

--- 19.Ex trabalho Ederson(1.3).java
+++ 19.Ex trabalho Ederson(1.3).java
@@ -5,14 +5,31 @@ import java.util.ArrayList;
 formatadas no console, com numeração. Demonstre uso de comentários e boas práticas. Extensão: Expanda para um app de
 produtividade em engenharia de software.*/
 
-public class Main{
+public class Main   {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] lista; //Aqui declara o vetor
-        lista=new String[]{"LImpar casa","Cuidar do cachorro","Lavar louça","Lavar roupa","Arrumar quarto"}; //aqui define os valores nele
+        ArrayList<String> Lista=new ArrayList<>();
         int i;
-        for(i=0;i<lista.length;i++){//aqui printa o vetor,com o I sendo o contador
-            System.out.printf("\n%d-%s",i+1,lista[i]);
+        String tarefas;
+        boolean aux=true;
+        System.out.print("\nSe quiser encerrar digite 0");
+        /*Esse while é pra entrar em loop para o usuario falar as tarefas,e quando digitar 0 sai do loop*/
+        while(aux){
+            System.out.print("\nFale a tarefa:");
+            tarefas=input.nextLine();
+            if(tarefas.equals("0")){
+                aux=false;
+                break;
+                /*Esse ponto equals verifica se o conteudo do objetos são iguais,
+                Ja o == ve se os dois valores apontam pro mesmo objeto*/
+            }
+            else{
+                Lista.add(tarefas);
+            }
+        }
+        System.out.printf("\nSua lista:");
+        for(i=0;i<Lista.size();i++){ //O size vem o tamanho da lista,e esse for imprime a lista com os numeros
+            System.out.printf("\n%d-%s",i+1,Lista.get(i)); //O get pega o valor na posição i no vetor
         }
     }
 }
