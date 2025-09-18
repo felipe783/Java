f9878a80ec2c279fe8db65acec99f4d9921edbee
felipe783 <felipebolt2007@Gmail.com>
2025-09-18 09:39:48 -0300

    Update 28.Ex trabalho Ederson(1.12).java

--- 28.Ex trabalho Ederson(1.12).java
+++ 28.Ex trabalho Ederson(1.12).java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/*Escolhedor de Roupas por Clima: Peça temperatura e clima (sol/chuva), use condicionais para sugerir roupas. Aninhe com
+switch para estações. Extensão: Relacione a apps de lifestyle ou IA pessoal.*/
 public class Main{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
@@ -13,27 +15,27 @@ public class Main{
         switch(clima){ //Ve o clima
             case 1: //Sol
                 if(temp<0){
-
+                    System.out.println("Calça,blusa");
                 }
                 else{
                     if(temp>0 && temp<=20){
-
+                        System.out.println("Calça,camisa");
                     }
                     else{ //Maior q 20
-
+                        System.out.println("Shorts,Camiseta");
                     }
                 }
                 break;
             case 2: //Chuva
                 if(temp<0){
-
+                    System.out.println("Calça,Blusa");
                 }
                 else{
                     if(temp>0 && temp<=20){
-
+                        System.out.println("Calça,Blusa");
                     }
                     else{ //Maior q 20
-
+                        System.out.println("Calça,camiseta");
                     }
                 }
                 break;
